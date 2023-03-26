356b69f107e8ef83003d34083ed04a449a91b051
Martin Dimitrov <52877024+mark79-github@users.noreply.github.com>
2023-03-26 13:52:07 +0300

    Create add_hosts.sh

---
+++ provision-scripts/add_hosts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "192.168.99.201 pipelines.do1.exam jenkins" >> /etc/hosts
+echo "192.168.99.202 containers.do1.exam docker" >> /etc/hosts
+echo "192.168.99.203 monitoring.do1.exam monitoring" >> /etc/hosts
