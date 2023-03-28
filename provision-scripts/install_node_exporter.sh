822cb20c1b93e861967958a0fcdfd2194891e0c5
Martin Dimitrov <52877024+mark79-github@users.noreply.github.com>
2023-03-28 10:37:45 +0300

    Update install_node_exporter.sh

--- provision-scripts/install_node_exporter.sh
+++ provision-scripts/install_node_exporter.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-echo "* Download node exporter"
-wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
+if [ -f "node_exporter-1.5.0.linux-amd64.tar.gz" ]; then
+    echo "*** Node exporter binary files found ..."
+else 
+    echo "* Download node exporter"
+    wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
+fi
 
-echo "* Extract node exporter"
-tar xzvf node_exporter-1.5.0.linux-amd64.tar.gz
+if [ -d "node_exporter-1.5.0.linux-amd64/" ]; then
+    echo "*** Node exporter directory found ..."
+else 
+    echo "* Extract node exporter"
+    tar xzvf node_exporter-1.5.0.linux-amd64.tar.gz
+fi
 
 echo "* Start node exporter"
 cd node_exporter-1.5.0.linux-amd64/ && ./node_exporter &> /tmp/node-exporter.log &
